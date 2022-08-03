41782548dc3bdfddea1e9ff93990e9c6acf6416f
jaleel <jasonlamdarksouls@gmail.com>
2022-08-03 09:41:06 +0100

    Enable Click on Grid
    Separated logic into individual modules. GameBoard array in own module,
    function to display gameboard in HTML in another module. Created
    function to remove previous gameboard. Game module calls these
    functions.

--- script.js
+++ script.js
@@ -1,47 +1,98 @@
 const wrapperElement = document.getElementById('wrapper');
 
+
 const gameBoardObject = (() => {
-    let space = '..';
-    let xShape = 'X';
-    let oShape = 'O';
-    const gameBoard = [[xShape,'|',space,'|',space,],
-                        ['-','-','-','-','-',],
-                        [xShape,'|',oShape,'|',space,],
-                        ['-','-','-','-','-',],
-                        [space,'|',space,'|',oShape,],
-                        ];
 
-    const displayGameBoard = () => {
+    const gameBoardOld = [" "," "," "," "," "," "," "," "," ",];
+    const gameBoardCurrent = ["x","x","x","x","x","x","x","x","x",];
+    const gameBoard = ["1","2","3","4","5","6","7","8","9",];
 
-        for (row of gameBoard) {
-            const rowElement = document.createElement('div');
-            
-            for (space of row) {
-                rowElement.append(space);
 
-            }
-            wrapperElement.appendChild(rowElement);
-            console.log(row);
-        }
+    const getBoardIndex = (index) => {
+        console.log(gameBoard[index]);
+    }
+    const setCell = (marker, index) => {
+        gameBoard[index] = marker;
+        console.log(gameBoard);
 
     }
-    return {displayGameBoard};
+    return {gameBoard, getBoardIndex, setCell};
 })();
 
 const displayController = (() => {
 
+    const gameBoard = gameBoardObject.gameBoard
+
+    const displayGameBoard = () => {
+
+        removePreviousGameBoard();
+        for (space of gameBoard) {
+            const spaceElement = document.createElement('div');
+            spaceElement.className = "clickable-space";
+            spaceElement.textContent = space;
+            wrapperElement.appendChild(spaceElement);
+        }
+    }
+
+    const removePreviousGameBoard = () => {
+
+        // if (wrapperElement.firstChild) {
+        //     for (child of (wrapperElement.children)) {
+        //         child.parentNode.removeLastChild(child);
+        //     }
+        // }
+        while (wrapperElement.lastChild) {
+            wrapperElement.removeChild(wrapperElement.lastChild);
+        }
+        console.log(wrapperElement);
+
+    }
+    return {displayGameBoard};
+
 })();
 
-const players = () => {
+const player = (name, marker) => {
+
     const playTurn = () => {
         console.log("place turned");
     }
     return {
-        playTurn
+        name,marker, playTurn
     };
 }
 
-gameBoardObject.displayGameBoard();
 
-const player1 = players();
-player1.playTurn();
+
+
+const game = (() => {
+    player1 = player("jaleel","x");
+    player2 = player("hm","o");
+
+    let currentPlayer = player1;
+
+    displayController.displayGameBoard();
+
+    const spaces = document.querySelectorAll(".clickable-space");
+
+    spaces.forEach((e, index) => {
+        e.addEventListener('click', function(event) {
+            gameBoardObject.getBoardIndex(index);
+            gameBoardObject.setCell(currentPlayer.marker, index);
+
+            console.log(gameBoardObject.gameBoard);
+
+            displayController.displayGameBoard();
+
+        })
+    });
+
+
+    console.log(player1.marker)
+    console.log("hehe pinf");
+    return {
+        player1, player2, gameBoardObject
+    };
+
+})();
+
+game;
