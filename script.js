8e071fe04ff1c74f2e9fcf2cd71e8a69937f5ac7
jaleel <jasonlamdarksouls@gmail.com>
2022-08-07 18:44:47 +0100

    Style Page and Style Current Cell
    Improved layout and decoration of page. Implemented targeting the
    current cell with classes and targeting the grid-container with
    index.

--- script.js
+++ script.js
@@ -1,45 +1,85 @@
-const wrapperElement = document.getElementById('wrapper');
+const gridContainer = document.getElementById('grid-container');
 
+const player1NameInput = document.getElementById("player1-name-input");
+const player2NameInput = document.getElementById("player2-name-input");
+const playButton = document.getElementById("play-button");
+
+let player1NameText = document.getElementById('player1-name-text');
+let player2NameText = document.getElementById('player2-name-text');
+
+playButton.addEventListener('click', function(e) {
+
+    e.preventDefault();
+
+    game.gameStart = true;
+
+    game.player1.name = player1NameInput.value ? player1NameInput.value : "Player 1";
+    game.player2.name = player2NameInput.value ? player2NameInput.value : "Player 2";
+
+    player1NameInput.style.display = 'none';
+    player2NameInput.style.display = 'none';
+
+    player1NameText.style.display = 'inline-block';
+    player2NameText.style.display = 'inline-block';
+
+    player1NameText.textContent = game.player1.name;
+    player2NameText.textContent = game.player2.name;
+
+    const gameTextContainer = document.getElementById('game-text-container');
+    gameTextContainer.style.display = 'grid';
+
+    playButton.style.display = 'none';
+
+})
 
 const gameBoardObject = (() => {
 
-    const gameBoard = Array(9).fill("");
-    const gameBoardCurrent = ["x","x","x","x","x","x","x","x","x",];
-    const gameBoard3 = ["1","2","3","4","5","6","7","8","9",];
+    let gameBoard = Array(9).fill("");
 
     const setCell = (board, marker, index) => {
         board[index] = marker;
-        // return(board);
     }
 
     const getBoard = () => {
         return [...gameBoard];
     } 
-    return {getBoard, setCell};
+
+    const resetBoard = () => {
+        gameBoard = Array(9).fill("");
+    }
+    return {getBoard, setCell, resetBoard};
 })();
 
 const displayController = (() => {
-
 
     const displayGameBoard = (board) => {
 
         removePreviousGameBoard();
         for (const space of board) {
             const spaceElement = document.createElement('div');
-            spaceElement.className = "clickable-space";
-            spaceElement.textContent = space;
-            wrapperElement.appendChild(spaceElement);
+            // spaceElement.className = "clickable-space";
+            spaceElement.classList.add("clickable-space");
+            textContainer = document.createElement('div');
+            textContainer.className = "text-container";
+            textContainer.textContent = space;
+            spaceElement.appendChild(textContainer);
+            gridContainer.appendChild(spaceElement);
         }
     }
 
     const removePreviousGameBoard = () => {
 
-        while (wrapperElement.lastChild) {
-            wrapperElement.removeChild(wrapperElement.lastChild);
+        while (gridContainer.lastChild) {
+            gridContainer.removeChild(gridContainer.lastChild);
         }
 
     }
-    return {displayGameBoard};
+
+    const declareCurrentCell = (index) => {
+        gridContainer.children[index].classList.add('current-cell');
+    }
+
+    return {displayGameBoard, declareCurrentCell};
 })();
 
 const player = (name, marker) => {
@@ -49,8 +89,8 @@ const player = (name, marker) => {
 }
 
 const game = (() => {
-    const player1 = player("jaleel","x");
-    const player2 = player("hm","o");
+    const player1 = player(player1NameInput.value,"x");
+    const player2 = player(player2NameInput.value,"o");
 
     let currentPlayer = player1;
 
@@ -58,63 +98,68 @@ const game = (() => {
         if (currentPlayer == player1 ? currentPlayer = player2 : currentPlayer = player1);
     }
 
-    const board = gameBoardObject.getBoard();
+    let board = gameBoardObject.getBoard();
 
     displayController.displayGameBoard(board);
 
-    // const spaces = document.querySelectorAll(".clickable-space");
-            gameContinue = true;
+    let gameStart = false;
+    let gameContinue = true;
 
 
-    document.addEventListener('click', function(e) {
-        if (e.target.className == 'clickable-space') {
-            let space = e.target;
-            let index = Array.from(space.parentNode.children).indexOf(space);
-
-        
+    // if (gameStart) {
+        document.addEventListener('click', function(e) {
+            // if (e.target.closest('.clickable-space').className == 'clickable-space') {
+            if (e.target.closest('.clickable-space').classList.contains('clickable-space')) {
+                
             
-            if (gameContinue) {
-                if (space.textContent == "") {
-                    gameBoardObject.setCell(board,currentPlayer.marker, index);
-                    displayController.displayGameBoard(board);
+                
+                let space = e.target.closest('.clickable-space');
+                let index = Array.from(space.parentNode.children).indexOf(space);
 
+                // removeCurrentCell(space.parentNode);
+            
+                if (gameContinue) {
+                    if (space.textContent == "") {
+                        gameBoardObject.setCell(board,currentPlayer.marker, index);
+                        displayController.displayGameBoard(board);
+                        displayController.declareCurrentCell(index);
+                        displayGameText();
+                        console.log(board);
 
-                    const indii = board.map((element, index) => {
-                        if (element == currentPlayer.marker) {
-                            return index;
+                        // logic that checks for wins
+                        const currentPlayerMarkerIndexes = board.map((element, index) => {
+                            if (element == currentPlayer.marker) {
+                                return index;
+                            };
+                        }).filter(element => element >= 0);
+
+                        const isSubset = (arr1, arr2) => {
+                            return arr2.every((element) => arr1.includes(element));
                         };
-                    }).filter(element => element >= 0);
 
-                    const isSubset = (arr1, arr2) => {
-                        return arr2.every((element) => arr1.includes(element));
-                    };
+                        const checkWin = () => winningCombinationsObject.winningCombinations.some((e) => {
+                            return isSubset(currentPlayerMarkerIndexes, e);
+                        });
 
-                    // for (combination of winningCombinationsObject.winningCombinations) {
-                    //     if (isSubset(indii, combination)) {
-                    //         console.log(`${currentPlayer.name} won!`);
-                    //         gameContinue = false;
-                    //     };
-                    // }
-                    const thisIsTheOne = () => winningCombinationsObject.winningCombinations.some((e) => {
-                        return e.every((element) => indii.includes(element));
-                    });
+                        if (checkWin()) {
+                            gameContinue = false;
+                            createPopup('win');
+                        } else {
+                            
+                            if (!board.includes("")) {
+                                gameContinue = false;
+                                createPopup('tie');
+                            }
+                        }
 
-                    if (thisIsTheOne()) {
-                        gameContinue = false;
+                        changePlayer();
                     }
 
-                    // for (combination of winningCombinationsObject.winningCombinations) {
-                    //     if (isSubset(indii, combination)) {
-                    //         console.log(`${currentPlayer.name} won!`);
-                    //         gameContinue = false;
-                    //     };
-                    // }
-                    changePlayer();
                 }
-
             }
-        }
-    });
+        });
+    // }
+
 
     const winningCombinationsObject = (() => {
         const winningCombinations = [
@@ -127,18 +172,77 @@ const game = (() => {
             [0,4,8],
             [2,4,6],
         ];
-
         return {winningCombinations}
     })();
-    // spaces.forEach((e, index) => {
-    //     e.addEventListener('click', function(event) {
-    //         gameBoardObject.setCell(board, player1.marker, index);
-    //         console.log(board);
 
-    //         displayController.displayGameBoard(board);
+    const createPopup = (gameOutcome) => {
+        const popupContainer = document.createElement('div');
+        popupContainer.id = 'popup-container';
 
-    //         changePlayer();
-    //         console.log(currentPlayer.marker);
-    //     });
-    // });
+        popupInnerContainer = document.createElement('div');
+        popupInnerContainer.id = 'popup-inner-container';
+        
+        popupContainer.appendChild(popupInnerContainer);
+
+        const gameResultText = document.createElement('p');
+
+        const gameTextContainer = document.getElementById('game-text-container');
+        gameTextContainer.style.display = 'none';
+
+        if (gameOutcome == 'win') {
+            gameResultText.textContent = `${currentPlayer.name} WINS`;
+        } else if (gameOutcome == 'tie') {
+            gameResultText.textContent = "It's a tie!";
+        }
+
+        popupInnerContainer.appendChild(gameResultText);
+
+        const playAgainButton = document.createElement('button');
+        playAgainButton.id = 'play-again-button';
+        playAgainButton.textContent = "Play Again?";
+        //logic when restarting game
+        playAgainButton.addEventListener('click', () => {
+            gameBoardObject.resetBoard();
+            
+            // reset gameboard
+            board = gameBoardObject.getBoard();
+            displayController.displayGameBoard(board);
+            gameContinue = true;
+
+            // display/hide html
+            playButton.style.display= 'inline-block';
+            body.removeChild(popupContainer);
+            player1NameInput.style.display = 'inline-block';
+            player2NameInput.style.display = 'inline-block';
+
+            player1NameText.style.display = 'none';
+            player2NameText.style.display = 'none';
+
+            player1NameInput.value = "";
+            player2NameInput.value = "";
+
+
+
+
+        })
+        popupInnerContainer.appendChild(playAgainButton);
+
+
+        const body = document.querySelector('body');
+        body.appendChild(popupContainer);
+    }
+
+    const currentTurnDiv= document.getElementById('current-turn-div');
+    const currentMarkerDiv= document.getElementById('current-marker-div');
+
+    const displayGameText = () => {
+        currentTurnDiv.textContent =`${currentPlayer.name}'s turn`;
+        currentMarkerDiv.textContent =`${currentPlayer.marker}`;
+    }
+    // createPopup();
+
+    return {player1, player2, gameStart}
 })();
+
+
+
